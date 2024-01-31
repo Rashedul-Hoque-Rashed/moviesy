de1a4a412080cecf5f94c976447758be8eb5cb4f
Rashed <rashedulhoqueceo@gmail.com>
2024-02-01 03:53:11 +0600

    create movies

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Movies from "../Movies/Movies";
 
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Movies />
         </div>
     );
 };
---
+++ src/Pages/Home/Movies/Movies.jsx
@@ -0,0 +1,16 @@
+import './Movies.css';
+
+const Movies = () => {
+    return (
+        <div>
+            <h2 className="text-center fs-2 fw-bolder title pt-5">New Movies</h2>
+            <div>
+                {
+                    
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Movies;
